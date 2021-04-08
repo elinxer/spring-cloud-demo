98f8cf4fb1b134b718fed516ea96dbb6322c1854
elinx <654753115@qq.com>
2021-04-08 15:36:10 +0800

    add super demo

--- src/main/java/com/zhiteer/demo/HelloController.java
+++ src/main/java/com/zhiteer/demo/HelloController.java
@@ -4,11 +4,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
 
-    @RequestMapping("/hello")
+    @RequestMapping("/index")
     public String index() {
         return "hello word!";
+    }
+
+    @RequestMapping("/test")
+    public String test() {
+
+
+
+        return "test";
     }
 
 }
